620e5dd2fa38bab3e912787374e31348be333dba
Hash <55654701+HasheDev@users.noreply.github.com>
2022-05-15 23:22:48 -0300

    Do used not

--- src/Api.js
+++
@@ -1,9 +0,0 @@
-export const getDollar = async (coin) =>{
-    try {
-        let url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL'
-        const res = await fetch(url)
-        return res.json
-    } catch (error) {
-        console.log(error)
-    }
-}
